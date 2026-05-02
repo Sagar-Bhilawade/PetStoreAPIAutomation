000fa0bfc3061da7e6382146acb855309ed40205
Sagar-Bhilawade <bhilawadesagar321@gmail.com>
2026-05-02 16:51:55 +0530

    Added Extent Report and Log4j

--- petstore/src/test/java/com/petstore/api/tests/UserTests.java
+++ petstore/src/test/java/com/petstore/api/tests/UserTests.java
@@ -1,5 +1,7 @@
 package com.petstore.api.tests;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,9 +16,11 @@ import io.restassured.response.Response;
 public class UserTests {
     Faker faker;
     User userPayload;
+    public Logger logger;
 
     @BeforeClass
     public void setup() {
+        logger = LogManager.getLogger(this.getClass());
         faker = new Faker();
         userPayload = new User();
         userPayload.setId(faker.idNumber().hashCode());
@@ -27,66 +31,82 @@ public class UserTests {
         userPayload.setPassword(faker.internet().password());
         userPayload.setPhone(faker.phoneNumber().cellPhone());
         userPayload.setUserStatus(1);
+        logger.info("Test data initialized for user: {}", userPayload.getUsername());
+
     }
 
     @Test(priority = 1)
     public void testCreateUser() {
-        // Implement the test for creating a user
+        logger.info("Creating user with username: {}", userPayload.getUsername());
         Response response = UserEndPoints.createUser(userPayload);
         response.then().log().all();
+        logger.info("Create user response status: {}", response.getStatusCode());
         Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("User created successfully: {}", userPayload.getUsername());
 
     }
 
     @Test(priority = 2, dependsOnMethods = "testCreateUser")
     public void testGetUserByUsername() {
-        // Implement the test for getting a user by username
+        logger.info("Fetching user by username: {}", userPayload.getUsername());
         Response response = UserEndPoints.getUser(userPayload.getUsername());
         response.then().log().all();
+        logger.info("Get user response status: {}", response.getStatusCode());
         Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("User fetched successfully: {}", userPayload.getUsername());
     }
 
     @Test(priority = 3, dependsOnMethods = "testCreateUser")
     public void testUpdateUser() {
-        // Implement the test for updating a user
+        logger.info("Updating user details for username: {}", userPayload.getUsername());
         userPayload.setFirstName(faker.name().firstName());
         userPayload.setLastName(faker.name().lastName());
         Response response = UserEndPoints.updateUser(userPayload.getUsername(), userPayload);
         response.then().log().all();
+        logger.info("Update user response status: {}", response.getStatusCode());
         Assert.assertEquals(response.getStatusCode(), 200);
-        // Verify that the user details are updated
+        logger.info("Verifying updated details for username: {}", userPayload.getUsername());
         Response getResponse = UserEndPoints.getUser(userPayload.getUsername());
+        logger.info("Verification response status: {}", getResponse.getStatusCode());
         Assert.assertEquals(getResponse.jsonPath().getString("firstName"), userPayload.getFirstName());
         Assert.assertEquals(getResponse.jsonPath().getString("lastName"), userPayload.getLastName());
         Assert.assertEquals(getResponse.jsonPath().getString("email"), userPayload.getEmail());
+        logger.info("User details updated successfully for username: {}", userPayload.getUsername());
     }
 
     @Test(priority = 4, dependsOnMethods = "testCreateUser")
     public void testLoginUser() {
-        // Implement the test for logging in a user
+        logger.info("Logging in user: {}", userPayload.getUsername());
         Response response = UserEndPoints.loginUser(userPayload.getUsername(), userPayload.getPassword());
         response.then().log().all();
+        logger.info("Login response status: {}", response.getStatusCode());
         Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("User logged in successfully: {}", userPayload.getUsername());
     }
 
     @Test(priority = 5, dependsOnMethods = "testLoginUser")
     public void testLogoutUser() {
-        // Implement the test for logging out a user
+        logger.info("Logging out current user session");
         Response response = UserEndPoints.logoutUser();
         response.then().log().all();
+        logger.info("Logout response status: {}", response.getStatusCode());
         Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("User logged out successfully");
     }
 
     @Test(priority = 6, dependsOnMethods = "testCreateUser")
     public void testDeleteUser() {
-        // Implement the test for deleting a user
+        logger.info("Deleting user: {}", userPayload.getUsername());
         Response response = UserEndPoints.deleteUser(userPayload.getUsername());
         response.then().log().all();
+        logger.info("Delete user response status: {}", response.getStatusCode());
         Assert.assertEquals(response.getStatusCode(), 200);
-        // Verify that the user is deleted
+        logger.info("Verifying deletion for username: {}", userPayload.getUsername());
         Response getResponse = UserEndPoints.getUser(userPayload.getUsername());
         getResponse.then().log().all();
+        logger.info("Post-delete fetch response status: {}", getResponse.getStatusCode());
         Assert.assertEquals(getResponse.getStatusCode(), 404);
+        logger.info("User deleted successfully: {}", userPayload.getUsername());
     }
 
 }
